63caf855101280a26cfb63d9366ac735bf5842eb
mia-chou <mia.fy.chou@hotmail.com>
2024-02-17 01:58:26 +0000

    Deleted my-app folder and changed weather app background

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,31 +1,33 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 
-export default function WeatherInfo (props) {
-    return (
+export default function WeatherInfo(props) {
+  return (
     <div className="WeatherInfo">
-<h1>{props.data.city}</h1>
-  <ul>
-    <li>
-      <FormattedDate date={props.data.date} />
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
         </li>
-    <li className="text-capitalize">{props.data.description}</li>
-  </ul>
-  <div className="row mt-3">
-    <div className="col-6">
-      <div className="clearfix float-left">
-      <img src={props.data.iconUrl} alt={props.data.description} />
-      <WeatherTemperature celsius={props.data.temperature} />
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix float-left d-flex justify-content-center">
+            <img src={props.data.iconUrl} alt={props.data.description} />
+            <span className="temperature">
+              {props.data.temperature}
+            </span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Feels like: {Math.round(props.data.feelsLike)}°C </li>
+            <li>Humidity: {Math.round(props.data.humidity)}%</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+          </ul>
+        </div>
       </div>
     </div>
-    <div className="col-6">
-      <ul>
-        <li>Feels like: {Math.round(props.data.feelsLike)}°C </li>
-        <li>Humidity: {Math.round(props.data.humidity)}%</li>
-        <li>Wind: {Math.round(props.data.wind)} km/h</li>
-      </ul>
-    </div>
-  </div>
-    </div>
-    );
+  );
 }
