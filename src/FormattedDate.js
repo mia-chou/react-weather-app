67407102b78e371376fba46b1d22e3b34221e497
mia-chou <mia.fy.chou@hotmail.com>
2024-02-21 05:19:21 +0000

    Updated WeatherFoecast.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -11,7 +11,7 @@ export default function FormattedDate(props){
         "Saturday"
       ];
 
-      let day = days[props.date.getDay()];
+    let day = days[props.date.getDay()];
 
 return (
   <div>
